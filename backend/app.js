b49941bd4b8031e82643b4329ea5f3d8f047a4f1
ANMOL <anmol.singh2843@gmail.com>
2023-07-25 17:07:42 +0530

    Fifth commit

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.get("/", function (req, res) {
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.port);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
